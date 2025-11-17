ec8e81c5859c4e4f415855e492c0b41a9dec8094
skdtdn <110184357+skdtdn@users.noreply.github.com>
2025-11-17 21:39:59 +0530

    Payments feat: integrate Polar SDK for subscription management and authentication
    - Added @polar-sh/better-auth and @polar-sh/sdk dependencies to package.json and package-lock.json.
    - Implemented subscription hooks: useSubscription and useHasActiveSubscription for managing user subscriptions.
    - Updated auth-client to utilize Polar SDK for authentication and checkout processes.
    - Enhanced AppSidebar to conditionally display the "Upgrade to Pro" button based on subscription status.
    - Introduced premiumProcedure to enforce premium membership for specific TRPC routes.
    - Refactored layout and app-header components for improved styling and structure.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import  prisma  from "@/lib/db";
+import { polarClient }from "./polar";
 
 
 export const auth = betterAuth({
@@ -11,4 +13,23 @@ export const auth = betterAuth({
         enabled: true,
         autoSignIn: true,
     },
+    plugins: [
+        polar({
+            client: polarClient,
+            createCustomerOnSignUp: true,
+            use: [
+                checkout({
+                    products: [
+                        {
+                            productId: "b48cddcb-4fa1-4353-b823-f6a647f203b8",
+                            slug: "Premium",
+                        }
+                    ],
+                    successUrl: process.env.POLAR_SUCCESS_URL,
+                    authenticatedUsersOnly: true,
+                }),
+                portal()
+            ],
+        })
+    ]
 });
